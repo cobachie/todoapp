b7651d28d09e4cfc9ce7658daf8d3e96b8b2a909
cobachie <7006340+cobachie@users.noreply.github.com>
2020-03-24 14:43:23 +0900

    add todo item to list with EventEmitter

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
-import { element } from "./view/html-util.js";
+import { TodoListModel } from "./model/TodoListModel.js";
+import { TodoItemModel } from "./model/TodoItemModel.js";
+import { element, render } from "./view/html-util.js";
 
 export class App {
+  constructor() {
+    this.todoListModel = new TodoListModel();
+  }
+
   mount () {
     const formElement = document.querySelector("#js-form");
     const inputElement = document.querySelector("#js-form-input");
     const containerElement = document.querySelector("#js-todo-list");
     const todoItemCountElement = document.querySelector("#js-todo-count");
 
-    let todoItemCount = 0;
+    // TodoListModel の状態が更新されたら表示を更新する
+    this.todoListModel.onChange(() => {
+      const todoListElement = element`<ul />`;
+
+      const todoItems = this.todoListModel.getTodoItems();
+      todoItems.forEach(item => {
+        const todoItemElement = element`<li>${item.title}</li>`;
+        todoListElement.appendChild(todoItemElement);
+      });
+
+      render(todoListElement, containerElement);
+
+      todoItemCountElement.textContent = `Todoアイテム数: ${this.todoListModel.getTotalCount()}`;
+
+    });
+
+    // フォームを送信したら新しいTodoItemModelを追加する
+    // let todoItemCount = 0;
     formElement.addEventListener("submit", (event) => {
       // submit イベントの本来の動作をとめる
       event.preventDefault();
 
-      // 追加するTodoアイテムの要素(li要素)を作成する
-      const todoItemElement = element`<li>${inputElement.value}</li>`;
-
-      // Todoアイテムをコンテナに追加する
-      containerElement.appendChild(todoItemElement);
-
-      todoItemCount += 1;
-      todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
-
+      this.todoListModel.addTodo(new TodoItemModel({
+        title: inputElement.value,
+        completed: false
+      }));
       inputElement.value = "";
-
-      console.log(`入力欄の値: ${inputElement.value}`);
     });
   }
 }
